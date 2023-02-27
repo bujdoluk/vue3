33730fe76b68703d9c4e77a763eda2c305b85911
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2023-02-27 23:25:11 +0100

    feat: firestore rules + add song, playlistdetail compoentn

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -12,7 +12,15 @@
     </div>
 
     <div class="song-list">
-      <p>song list here</p>
+      <div v-if="!playlist.songs.length">No songs have been added to this playlist yet</div>
+      <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+        <div class="details">
+          <h3>{{ song.title }}</h3>
+          <p>{{ song.artist }}</p>
+        </div>
+        <button v-if="ownership">delete</button>
+      </div>
+      <AddSong v-if="ownership" :playlist="playlist"/>
     </div>
   </div>
 </template>
@@ -22,20 +30,28 @@ import getDocument from '@/composables/getDocument';
 import getUser from '@/composables/getUser';
 import { computed } from 'vue';
 import useDocument from '@/composables/useDocument';
+import useStorage from '@/composables/useStorage';
+import { useRouter } from 'vue-router';
+import AddSong from '@/components/AddSong.vue';
 
 export default {
     props: ['id'],
+    components: { AddSong },
     setup(props) {
       const { error, document:playlist } = getDocument('playlists', props.id);
       const { user } = getUser();
       const { deleteDoc } = useDocument('playlists', props.id);
+      const { deleteImage } = useStorage();
+      const router = useRouter();
 
       const ownership = computed(() => {
         return playlist.value && user.value && user.value.uid == playlist.value.userId;
       });
 
       const handleDelete = async () => {
+        await deleteImage(playlist.value.filePath);
         await deleteDoc();
+        router.push({ name: 'Home' });
       }
 
       return { error, playlist, ownership, handleDelete };
@@ -81,5 +97,13 @@ export default {
 }
 .description {
   text-align: left;
+}
+.single-song {
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px dashed var(--secondary);
+  margin-bottom: 20px;
 }
 </style>
